aa4828c65e651c2ae55ada7a7a7d51f60a589d24
Chace Hartwell <chace.l.hartwell@icloud.com>
2018-08-05 11:43:19 -0600

    finished items, added some finishing touches

--- game/game.js
+++ game/game.js
@@ -1,7 +1,26 @@
 const blackKnight = {
     name: 'Black Knight',
     health: 100,
-    hits: 0
+    hits: 0,
+    items: {}
+}
+
+const items = {
+    potion: {
+        name: 'Health Potion',
+        modifier: 25,
+        description: 'Heals opponent by 25hp'
+    },
+    shield: {
+        name: 'Shield',
+        modifier: -.5,
+        description: 'Decreases dmg output by 50%'
+    },
+    curse: {
+        name: 'Curse',
+        modifier: .5,
+        description: 'Increases dmg output by 50%'
+    }
 }
 
 let enemy = document.getElementById('name')
@@ -9,59 +28,80 @@ let enemy = document.getElementById('name')
 const enemyName = document.getElementById('name')
 const health = document.getElementById('health')
 const hits = document.getElementById('hits')
+const activeItem = document.getElementById('active-item')
 const slapButton = document.getElementById('slap')
 const punchButton = document.getElementById('punch')
 const kickButton = document.getElementById('kick')
+const item1Button = document.getElementById('item1')
+const item2Button = document.getElementById('item2')
+const item3Button = document.getElementById('item3')
 
 function draw() {
+    endGame()
     enemy.innerText = blackKnight.name
     health.innerText = blackKnight.health.toString()
     hits.innerText = blackKnight.hits.toString()
+    activeItem.innerText = blackKnight.items.description || 'No Item'
 }
 
+function addItem(name) {
+    switch (name) {
+        case 'potion':
+            increaseHealth(items.potion.modifier)
+            break
+        default:
+            blackKnight.items = items[name]
+    } draw()
+}
 
-function slap() {
-    blackKnight.health--;
-    blackKnight.hits++;
+function increaseHealth(hp) {
+    if (blackKnight.health + hp > 100) {
+        blackKnight.health = 100
+    } else {
+        blackKnight.health += hp
+    }
+}
+
+function decreaseHealth(hp) {
+    if (blackKnight.items.description) {
+        hp += hp * blackKnight.items.modifier
+        if (hp < 0) {
+            hp = 0
+        }
+    }
+    if (blackKnight.health - hp <= 0) {
+        blackKnight.health = 0
+    } else {
+        blackKnight.health -= hp
+    }
+}
+
+function endGame() {
     if (blackKnight.health == 0) {
         slapButton.disabled = true
-    }
-    if (blackKnight.health == 0) {
-        punchButton.disabled = true
-    }
-    if (blackKnight.health == 0) {
         kickButton.disabled = true
+        punchButton.disabled = true
+        item1Button.disabled = true
+        item2Button.disabled = true
+        item3Button.disabled = true
     }
+}
+
+function slap() {
+    decreaseHealth(1);
+    blackKnight.hits++;
     draw()
 }
 
 function punch() {
-    blackKnight.health -= 5
+    decreaseHealth(5)
     blackKnight.hits++;
-    if (blackKnight.health == 0) {
-        punchButton.disabled = true
-    }
-    if (blackKnight.health == 0) {
-        slapButton.disabled = true
-    }
-    if (blackKnight.health == 0) {
-        kickButton.disabled = true
-    }
     draw()
 }
 
 function kick() {
-    blackKnight.health -= 10
+    decreaseHealth(10)
     blackKnight.hits++;
-    if (blackKnight.health == 0) {
-        kickButton.disabled = true
-    }
-    if (blackKnight.health == 0) {
-        slapButton.disabled = true
-    }
-    if (blackKnight.health == 0) {
-        punchButton.disabled = true
-    }
     draw()
 }
 
@@ -71,6 +111,10 @@ function reset() {
     slapButton.disabled = false
     punchButton.disabled = false
     kickButton.disabled = false
+    item1Button.disabled = false
+    item2Button.disabled = false
+    item3Button.disabled = false
+    blackKnight.items = []
     draw()
 }
 
